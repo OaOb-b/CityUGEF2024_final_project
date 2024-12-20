3d857347e4c1cb9239cb881e7b73c1e642cea961
Paco <127317346+Tartuwu@users.noreply.github.com>
2024-12-20 22:11:22 +0800

    Update utilizing.py

--- YOLO/utilizing.py
+++ YOLO/utilizing.py
@@ -1,16 +1,20 @@
 from ultralytics import YOLO
 import tkinter
 
-top = tkinter.Tk()
-global classes
+
 def run():
-    model = YOLO('/Users/paco/Documents/GitHub/CityUGEF2024_final_project/YOLO/model/model.pt')  #change the path if needed (use model.pt)
+    model = YOLO('YOLO/model/model.pt')  #change the path if needed (use model.pt)
     classes = set()
     results = model(source=0, show= True, conf= 0.4, save= True)
     print(set(classes))
+    
+    
+top = tkinter.Tk()
 
-B = tkinter.Button(top, text ="press me", command = run)
+top.geometry("414x736")
+top.wm_resizable(False,False)
+
+B = tkinter.Button(top, text ="ENTER THE OBJECT DETECTION", command = run , bg = "skyblue")
 B.pack()
 top.mainloop()
 
-#print(set(classes))

3b699f6d1680626c19dc053d48c2b3d7b0c8c10b
Paco <127317346+Tartuwu@users.noreply.github.com>
2025-01-23 22:54:07 +0800

    Update class.py

--- class.py
+++ class.py
@@ -1,72 +1,119 @@
-# recipe_app.py
 import tkinter as tk
 from tkinter import messagebox
+import pandas as pd
+import numpy as np
+from ast import literal_eval
+import spacy
+from ultralytics import YOLO
 
-class RecipeApp:
-    def __init__(self, root, yolo_function, recommendation_function):
+class RecipeDataset:
+    def __init__(self, recipes_path, interactions_path):
+        self.df1 = pd.read_csv(recipes_path)
+        self.df2 = pd.read_csv(interactions_path)
+        self.prepare_data()
+
+    def prepare_data(self):
+        rating = self.df2.groupby('recipe_id')['rating'].mean()
+        rating = pd.DataFrame(rating)
+        rating.index.names = ['id']
+        self.df = self.df1.merge(rating, on='id')
+        self.df.nutrition = self.df.nutrition.apply(literal_eval)
+        self.df[['calories', 'total_fat (%DV)', 'sugar (%DV)', 
+                  'sodium (%DV)', 'protein (%DV)', 'saturated_fat (%DV)', 
+                  'total_carbohydrate (%DV)']] = list(n for n in self.df.nutrition)
+        self.df = self.df.drop(columns=['contributor_id', 'submitted', 
+                                          'tags', 'description', 'nutrition'])
+        self.df = self.df[self.df.name.notnull()]
+        self.df = self.df.sort_values(by='rating', ascending=False)
+
+class RecipeNLP:
+    def __init__(self, model_path):
+        self.nlp = spacy.load(model_path)
+
+    def extract_components(self, text):
+        doc = self.nlp(text)
+        component = {'ingredients': [], 'style': []}
+        for ent in doc.ents:
+            text = ent.text
+            if ent.label_ == 'ingredient':
+                if text.endswith('ies'):
+                    text = text[:-3] + 'y'
+                elif text.endswith('es'):
+                    text = text[:-2]
+                elif text.endswith('s'):
+                    text = text[:-1]
+                component['ingredients'].append(text)
+            elif ent.label_ == 'product':
+                component['style'].append(text)
+        return component
+
+class YOLOModel:
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
+
+    def run_detection(self):
+        results = self.model(source=0, show=True, conf=0.4, save=True)
+
+class RecipeRecommenderApp:
+    def __init__(self, root, dataset, nlp_model, yolo_model):
         self.root = root
+        self.dataset = dataset
+        self.nlp_model = nlp_model
+        self.yolo_model = yolo_model
+        self.input_text = ''
+        self.setup_ui()
+
+    def setup_ui(self):
         self.root.geometry("414x736")
         self.root.wm_resizable(False, False)
 
-        self.yolo_function = yolo_function
-        self.recommendation_function = recommendation_function
-
-        self.create_widgets()
-
-    def create_widgets(self):
-        # Button for object detection
-        self.detect_button = tk.Button(self.root, text="ENTER THE OBJECT DETECTION", command=self.run_yolo, bg="skyblue", width=30, height=5)
-        self.detect_button.pack()
-
-        # Label for ingredient input
-        self.ingredient_label = tk.Label(self.root, text="Do you have other ingredients? Please enter below", font=("Arial", 16))
-        self.ingredient_label.pack(pady=20)
-
-        # Entry for ingredients
+        tk.Button(self.root, text="ENTER THE OBJECT DETECTION", command=self.run_yolo, bg="skyblue", width=30, height=5).pack()
+        
+        tk.Label(self.root, text="Do you have other ingredients? Please enter below", font=("Arial", 16)).pack(pady=20)
+        
         self.entry = tk.Entry(self.root, font=("Arial", 14), width=40)
         self.entry.pack(pady=(10, 20))
-
-        # Submit button for ingredients
-        self.submit_button = tk.Button(self.root, text="Submit", command=self.on_submit, font=("Arial", 14), bg="#4CAF50", fg="black")
-        self.submit_button.pack(pady=20)
-
-        # Button for recipe recommendation
-        self.recipe_button = tk.Button(self.root, text="ENTER THE RECIPE RECOMMENDATION", command=self.run_res, bg="skyblue", width=30, height=5)
-        self.recipe_button.pack()
-
-        # Label for disease selection
-        self.disease_label = tk.Label(self.root, text="Do you have other diseases? Please select below", font=("Arial", 16))
-        self.disease_label.pack(pady=20)
-
-        # Listbox for disease selection
+        
+        tk.Button(self.root, text="Submit", command=self.on_submit, font=("Arial", 14), bg="#4CAF50", fg="black").pack(pady=20)
+        
+        tk.Button(self.root, text="ENTER THE RECIPE RECOMMENDATION", command=self.run_recommendation, bg="skyblue", width=30, height=5).pack()
+        
+        tk.Label(self.root, text="Do you have other diseases? Please select below", font=("Arial", 16)).pack(pady=20)
+        
         self.listbox = tk.Listbox(self.root, font=("Arial", 14), height=7, selectmode=tk.MULTIPLE)
         self.listbox.pack(pady=20)
-
-        # List of diseases
+        
         items = ["heart disease", "diabetes", "obesity", "high cholesterol", "hypertension", "chronic kidney disease", "bowel cancer"]
         for item in items:
             self.listbox.insert(tk.END, item)
-
+        
         self.listbox.bind('<<ListboxSelect>>', self.on_select)
 
-    def on_submit(self):
-        user_input = self.entry.get()
-        messagebox.showinfo("Information", "Your information has been saved")
-        return user_input
-
-    def on_select(self, event=None):
-        selected_indices = self.listbox.curselection()
-        selected_values = [self.listbox.get(i) for i in selected_indices]
-        print(f"You selected: {', '.join(selected_values)}")
-
     def run_yolo(self):
-        if self.yolo_function:
-            self.yolo_function()
-        else:
-            print("YOLO function is not defined.")
+        self.yolo_model.run_detection()
 
-    def run_res(self):
-        if self.recommendation_function:
-            self.recommendation_function()
-        else:
-            print("Recommendation function is not defined.")
+    def on_submit(self):
+        self.input_text = self.entry.get()
+        messagebox.showinfo("Information", "Your information has been saved.")
+
+    def run_recommendation(self):
+        components = self.nlp_model.extract_components(self.input_text)
+        filtered_recipes = self.to_query(components)
+        messagebox.showinfo("Information", filtered_recipes.to_string())
+
+    def to_query(self, component):
+        ingredients = component['ingredients']
+        filtered_df = self.dataset.df.copy()
+        for ingredient in ingredients:
+            filtered_df = filtered_df[filtered_df.ingredients.str.contains(f'{ingredient}', na=False)]
+        return filtered_df
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    dataset = RecipeDataset('/Users/paco/Downloads/Food.com Recipes/RAW_recipes.csv',
+                            '/Users/paco/Downloads/Food.com Recipes/RAW_interactions.csv')
+    nlp_model = RecipeNLP('/Users/paco/Documents/GitHub/CityUGEF2024_final_project/Query_sys/NER/output/model-best')
+    yolo_model = YOLOModel('/Users/paco/Documents/GitHub/CityUGEF2024_final_project/YOLO/model/model.pt')
+
+    app = RecipeRecommenderApp(root, dataset, nlp_model, yolo_model)
+    root.mainloop()

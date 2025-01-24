bc3c61a84bd3b3012db5562cf7c800055e4192e9
Paco <127317346+Tartuwu@users.noreply.github.com>
2025-01-24 15:31:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ import pandas as pd
 import numpy as np
 from ast import literal_eval
 import spacy 
+import cv2
+from time import time, sleep
+from datetime import datetime, timedelta
+from threading import Thread, Lock
+from collections import deque
+from ultralytics.solutions import object_counter
+import shelve
+import os
+from pathlib import Path
 
 global input
 
-# Query
+####-------------------------------recommemdation-------------------------------------*/
 df1 = pd.read_csv('/Users/paco/Downloads/Archive (1)/RAW_recipes.csv')     # Change the path if needed 
 df2 = pd.read_csv('/Users/paco/Downloads/Archive (1)/RAW_interactions.csv')    # Change the path if needed 
 nlp_NER = spacy.load('/Users/paco/Documents/GitHub/CityUGEF2024_final_project/Query_sys/NER/output/model-best') # Change the path if needed 
@@ -44,7 +53,35 @@ def to_query(df_, component):
     print(filtered_df)
     return filtered_df
 
-# YOLO
+def display_recipes(recipe):
+    # Create a new window for displaying recipes
+    recipe_window = Toplevel(top)
+    recipe_window.title("Recipe Recommendation")
+    recipe_window.geometry("500x600")  # Set the size of the window
+    
+    recipe_window.configure(bg='black')  # Set a dark background color
+
+    if not recipe.empty:
+        row = recipe.iloc[0]  # Select the first recipe
+        text_widget = Text(recipe_window, wrap="word", font=("Arial", 12), padx=10, pady=10, bg='black', fg='white')
+        text_widget.pack(fill=tkinter.BOTH, expand=True)
+
+        text_widget.insert(tkinter.END, f"Recipe Name: {row['name']}\n")
+        text_widget.insert(tkinter.END, f"Ingredients: {row['ingredients']}\n")
+        
+        # Split the cooking instructions into separate steps and display each step on a new line
+        steps = row['steps'].split('\n')
+        for step in steps:
+            text_widget.insert(tkinter.END, f"{step}\n")
+            
+    else:
+        text_widget = Text(recipe_window, wrap="word", font=("Arial", 12), padx=10, pady=10, bg='black', fg='white')
+        text_widget.pack(fill=tkinter.BOTH, expand=True)
+        text_widget.insert(tkinter.END, "No recipes found for the given ingredients.")
+        
+    text_widget.config(state=tkinter.DISABLED)
+
+####-------------------------------YOLO-------------------------------------*/
 model = YOLO('/Users/paco/Documents/GitHub/CityUGEF2024_final_project/YOLO/model/model.pt')  # Change the path if needed (use model.pt)
 classes = set()
 
@@ -77,34 +114,198 @@ def runRes():
     # Display recipes in a better output window
     display_recipes(recipe)
 
-def display_recipes(recipe):
-    # Create a new window for displaying recipes
-    recipe_window = Toplevel(top)
-    recipe_window.title("Recipe Recommendation")
-    recipe_window.geometry("500x600")  # Set the size of the window
+####-------------------------------fridge-------------------------------------*/
+
+mapping = ['Onion',
+'beef',
+'chicken',
+'eggs',
+'potato',
+'radish',
+'garlic',
+'lettuce',
+'tomato']
+
+FRIDGE = shelve.open('/Users/paco/Documents/GitHub/CityUGEF2024_final_project/YOLO/Fridge', writeback= True)
+
+class ObjectDetection:
+
+    def __init__(self, capture_index):
+       
+        self.capture_index = capture_index
+                
+        self.model = self.load_model()
+        print('??????')
     
-    recipe_window.configure(bg='black')  # Set a dark background color
+    def load_model(self):
+       
+        model = YOLO('/Users/paco/Documents/GitHub/CityUGEF2024_final_project/YOLO/model/best_1.pt')  # load a pretrained YOLOv8n model
+        model.fuse()
+    
+        return model
 
-    if not recipe.empty:
-        row = recipe.iloc[0]  # Select the first recipe
-        text_widget = Text(recipe_window, wrap="word", font=("Arial", 12), padx=10, pady=10, bg='black', fg='white')
-        text_widget.pack(fill=tkinter.BOTH, expand=True)
-
-        text_widget.insert(tkinter.END, f"Recipe Name: {row['name']}\n")
-        text_widget.insert(tkinter.END, f"Ingredients: {row['ingredients']}\n")
+    def predict(self, frame):
+       
+        results = self.model(frame)
         
-        # Split the cooking instructions into separate steps and display each step on a new line
-        steps = row['steps'].split('\n')
-        for step in steps:
-            text_widget.insert(tkinter.END, f"{step}\n")
+        return results
+    
+
+    def plot_bboxes(self, results, frame):
+        class_ids = []
+        
+         # Extract detections for person class
+        for result in results:
+            boxes = result.boxes.cpu().numpy()
+            class_ids.append(boxes.cls.astype(int))
+
+
+        return results[0].plot(), class_ids
+
+    def stock(self, ids):
+        ids = ids[0]
+        for Id in list(ids):
+            ingredient = INGREDIENT(Id, 1/4320, datetime.now())  # 20 seconds span
+            hashh = mapping[Id]
+            FRIDGE[hashh].append(ingredient)
+
+
+    def consume(self, ids):
+        ids = ids[0]
+        for Id in list(ids):
+            hashh = mapping[Id]
+            if FRIDGE[hashh]:
+                FRIDGE[hashh].popleft()
+                
+
+    def __call__(self, mode):
+        last_s = 0
+        cap = cv2.VideoCapture(self.capture_index)
+        
+        # Loop through the video frames
+        while cap.isOpened():
             
-    else:
-        text_widget = Text(recipe_window, wrap="word", font=("Arial", 12), padx=10, pady=10, bg='black', fg='white')
-        text_widget.pack(fill=tkinter.BOTH, expand=True)
-        text_widget.insert(tkinter.END, "No recipes found for the given ingredients.")
-        
-    text_widget.config(state=tkinter.DISABLED)
+
+            # Read a frame from the video
+            success, frame = cap.read()
+
+            if success:
+                # Run YOLO inference on the frame
+                results = self.predict(frame)
+
+                # Visualize the results on the frame
+                annotated_frame, ids = self.plot_bboxes(results, frame)
+                
+                p = Path('dummy_path')  # Create a dummy path object
+                im = frame  # The preprocessed image is the same as the original frame in this case
+                im0 = frame.copy()  # Create a copy of the frame for visualization
+                s = self.model.predictor.write_results(0, p, im, results)
+                s = s[s.find(' ', 3) + 1]
+
+                if s != last_s:
     
+                    if mode:
+                        self.stock(ids)
+                    else:
+                        self.consume(ids)
+                
+                last_s = s
+
+                 # Display the annotated frame
+                cv2.imshow("YOLO Inference", annotated_frame)
+
+                # Break the loop if 'q' is pressed
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+                
+            else:
+                break
+                
+        cap.release()
+        cv2.destroyAllWindows()
+        
+class INGREDIENT:
+    def __init__(self, Id, span, inbound_datetime):
+        self.id = Id
+        self.span = span
+        self.inbound_datetime = inbound_datetime
+        self.expiration_datetime = inbound_datetime + timedelta(days= span)
+
+    def get_id(self):
+        return self.id
+
+    def get_expiration(self):
+        return self.expiration_datetime
+                
+def get_stock(fridge):
+    hashmap = {}
+    for ingredients in dict(fridge).values():
+        for i in ingredients:
+            iid  = mapping[i.get_id()] 
+            
+            if iid not in hashmap:
+                hashmap[f'{iid}'] = 1
+            else:
+                hashmap[f'{iid}'] += 1
+        
+    print(hashmap)
+    
+    
+
+def main(fridge):
+    while True:
+        for ingredients in dict(fridge).values():
+            for ingredient in ingredients:
+                d = ingredient.get_expiration()
+                current = datetime.now()
+                if current >= d:
+                    print("*****************")
+                    print('expired')
+                    print("*****************")
+        sleep(7)
+
+
+#t = Thread(target= main, args= (FRIDGE, ))
+#t.daemon = True
+#t.start()
+
+def AskForInput():
+    choice = input('1: put things into the fridge\n2: get things out of the fridge\n3: show stocks of the fridge\n4: turn off the fridge\n5: empty the fridge (test mode cheat code)\n->')
+
+    if choice == '1':
+        ObjectDetection(1)(1)
+
+    elif choice == '2':
+        ObjectDetection(1)(0)
+
+    elif choice == '3':
+        os.system('cls||clear')
+        get_stock(FRIDGE) 
+        input('Press any to continue')
+
+    elif choice == '4':
+        FRIDGE.close()
+        return
+        
+    else:
+        FRIDGE['Onion'] = deque()
+        FRIDGE['beef'] = deque()
+        FRIDGE['chicken'] = deque()
+        FRIDGE['eggs'] = deque()
+        FRIDGE['potato'] = deque()
+        FRIDGE['radish'] = deque()
+        FRIDGE['garlic'] = deque()
+        FRIDGE['lettuce'] = deque()
+        FRIDGE['tomato'] = deque()
+    
+    # os.system('cls||clear')
+    
+    return 1
+
+#while AskForInput():
+#    pass
+
+####-------------------------------GUI-------------------------------------*/
 top = tkinter.Tk()
 
 def on_submit():
@@ -131,6 +332,22 @@ def on_select(event=None):
 
 B = tkinter.Button(top, text="ENTER THE OBJECT DETECTION", command=runYOLO, bg="skyblue", width=30, height=5)
 B.pack()
+
+button_frame = tkinter.Frame(top)
+button_frame.pack(pady=20)
+
+# Create and pack the buttons
+button1 = tkinter.Button(button_frame, text="Button 1", command=ObjectDetection(1)(1))
+button1.pack(side=tkinter.LEFT, padx=5)
+
+button2 = tkinter.Button(button_frame, text="Button 2", command=ObjectDetection(1)(0))
+button2.pack(side=tkinter.LEFT, padx=5)
+
+button3 = tkinter.Button(button_frame, text="Button 3", command=get_stock(FRIDGE) )
+button3.pack(side=tkinter.LEFT, padx=5)
+
+#button4 = tkinter.Button(button_frame, text="Button 4", command=print("4"))
+#button4.pack(side=tkinter.LEFT, padx=5)
 
 label = tkinter.Label(top, text="Do you have other ingredients? Please enter below:", font=("Arial", 16))  # Create a label with text
 label.pack(pady=20) 
